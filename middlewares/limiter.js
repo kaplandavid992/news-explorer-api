7ffc811baaf5dba7c67abf1870f6ea5d9b72c822
kaplandavid992 <kaplandavid992@gmail.com>
2022-08-10 01:16:43 +0300

    ty

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -3,7 +3,7 @@ const ForbiddenError = require('../errors/ForbiddenError');
 
 exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 20,
+  max: 100,
   standardHeaders: true,
   legacyHeaders: false,
   handler: () => {
--- utils/validateArticle.js
+++ utils/validateArticle.js
@@ -2,7 +2,7 @@ const { celebrate, Joi } = require('celebrate');
 
 const validateArticle = () => celebrate({
   body: Joi.object().keys({
-    keyword: Joi.string().required().min(2),
+    keyword: Joi.string().required(),
     title: Joi.string().required(),
     text: Joi.string().required().min(2),
     source: Joi.string().required(),
